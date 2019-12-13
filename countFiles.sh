ed971ece49d43302ca9b43e3ac8a1728f7bcb29d
kuldeep7895 <kuldeepsan09@gmail.com>
2019-12-13 21:33:24 +0530

    changed countFiles now only search in main directory no sub directory

--- countFiles.sh
+++ countFiles.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 #program takes two arguments directory path and type of file
+#maxdepth specifies the level upto which search need to be made
 #type specifies only files to be searched
 #name specifies the particular extension need be searched
-find $1 -type f -name "*"$2 | wc -l
+find $1 -maxdepth 1 -type f -name "*"$2 | wc -l
